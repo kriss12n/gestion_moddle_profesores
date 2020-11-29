92e2b9587474d6436140db4232988992cdccb2a9
oesi xd <andrespro529@gmail.com>
2020-11-29 16:49:11 -0300

    otro bug arreglado

--- application/models/UsuarioModel.php
+++ application/models/UsuarioModel.php
@@ -74,12 +74,14 @@ class UsuarioModel extends CI_Model
 	public function getAlumnosByFilter($id)
 	{
 		$this->db->select("user.id,user.rut, user.name,user.lastname_p,user.lastname_m
-		,user.address,user.commune,user.password, user.phone,user.email,user.prioritary,user.course_id,user.representative_id ,user.representative_supp_id,user.contact_movil ,user.rol_id,roles.name as rol ");
+		,user.address,user.commune,user.password, user.phone,user.email,user.prioritary,user.course_id,user.representative_id ,user.representative_supp_id,user.contact_movil ,user.rol_id,roles.name as rol ,representative.name as pepe ,representative.lastname_p as apellidop ,representative.lastname_m as lastnamem ,
+		,representative2.name as pepe2 ,representative2.lastname_p as apellidop2 ,representative2.lastname_m as lastnamem2 ,bs.name as course_idr ");
 		$this->db->from("user");
 			$this->db->join('roles', 'roles.id = user.rol_id', 'Left');
-		
-			
-		$this->db->where('rol_id',$id );
+			$this->db->join('user as representative', 'representative.id = user.representative_id','Left');	
+			$this->db->join('user as representative2', 'representative2.id = user.representative_supp_id','Left');	
+			$this->db->join('base_course as bs', 'bs.id = user.course_id','Left');	
+		$this->db->where('user.rol_id',$id );
 		$query = $this->db->get();
 		return $query->result();
 	}
